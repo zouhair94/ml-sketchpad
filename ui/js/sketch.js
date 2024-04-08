1504935d2583e9af598b5f99be72f140453a9433
zouhair ridaoui <z.ridaoui94@outlook.com>
2024-04-08 14:19:56 +0300

    feat(eventListeners): add event listeners

--- ui/js/sketch.js
+++ ui/js/sketch.js
@@ -11,6 +11,35 @@ class Sketch {
             flex: 1;
         `
         container.appendChild(this.canvas);
+        this.paths = [];
+        this.drawing = false;
+    }
+
+    #addEvenListeners() {
+        this.canvas.onmousedown = (e) => {
+            const mouse = this.#getMouse(e);
+            this.paths.push(mouse);
+            this.drawing = true;
+        }
+        this.canvas.onmousemove = (e) => {
+            if (this.drawing) {
+                const mouse = this.#getMouse(e);
+                const lastPath = this.paths[this.paths.length - 1];
+                lastPath.push(mouse);
+            }
+        }
+        document.onmouseUp = (e) => {
+            this.drawing = false;
+        }
+    }
+
+
+    #getMouse = (evt) => {
+        const rect = this.canvas.getBoundingClientRect();
+        return [
+            Math.round(evt.clientX - rect.left),
+            Math.round(evt.clientY - rect.top)
+        ];
     }
 
 }
