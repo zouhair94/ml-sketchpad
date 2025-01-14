361454680049d01d044253c861e7a52fe7e7ee44
zouhair ridaoui <z.ridaoui94@outlook.com>
2025-01-14 10:58:13 +0300

    feat(samples): add samples js

--- common/constants.js
+++ common/constants.js
@@ -6,6 +6,8 @@ constants.DATASET_DIR = constants.DATA_DIR + "/dataset";
 constants.JSON_DIR = constants.DATASET_DIR + "/json";
 constants.IMG_DIR = constants.DATASET_DIR + "/img";
 constants.SAMPLES = constants.DATASET_DIR + "/samples.json";
+constants.JS_OBJECT = "../../common/js_objects";
+constants.SAMPLES_JS = constants.JS_OBJECT + "/samples.js";
 
 if (typeof module !== "undefined") {
     module.exports = constants;
